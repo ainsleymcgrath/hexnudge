e2355424994d633ae2e6554b5f56882a27089790
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2018-09-13 10:21:18 -0500

    better tests for new application code
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,7 +1,13 @@
 import "jest";
-import { nudgeToValidHexPart } from "../components/_helpers";
+import {
+  nudgeToValidHexPart,
+  isNumber,
+  display,
+  keepMaxMin
+} from "../components/_helpers";
 
-describe("The nudgeToValidHexPart function", () => {
+xdescribe("The nudgeToValidHexPart function", () => {
+  // will probably get rid of this guy
   it("Returns the initial value for when given a non-integer (decimal)", () => {
     expect(nudgeToValidHexPart("FF", 1.1)).toEqual("FF");
   });
@@ -24,4 +30,17 @@ describe("The nudgeToValidHexPart function", () => {
   it("Returns 0 for nudge to below 00", () => {
     expect(nudgeToValidHexPart("FF", -256)).toEqual("00");
   });
+});
+
+describe("The isNumber function", () => {
+  it("says false when not a number", () => {
+    expect(isNumber("doodah")).toBe(false);
+  });
+
+  it("returns true when it is a number", () => {
+    expect(isNumber(5)).toBe(true);
+  });
+
+  // TODO: checks for strings
+  // TODO: hecks for base 16 and base 10 numbers
 });
